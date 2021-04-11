607ae15f38d8184d29529f8716f5f679a8f60f8c
kfirmanty <karol.firmanty@gmail.com>
2021-04-11 21:16:57 +0200

    messing with keyboard handling and adding cursor

--- vague_conductor.lua
+++ vague_conductor.lua
@@ -5,6 +5,7 @@ local keycodes = include('lib/keycodes')
 include('lib/parser')
 
 local text = 'play 6 short notes'
+local cursor = string.len(text)
 local enter_text_mode = true
 local commands = {}
 local clock_id = -1
@@ -49,6 +50,10 @@ local function get_key(code, val, shift)
     end
 end
 
+function string.insert(str1, str2, pos)
+    return str1:sub(1,pos)..str2..str1:sub(pos+1)
+end
+
 function keyb.event(typ, code, val)
    local menu = norns.menu.status()
    local k = get_key(code, val, shift)
@@ -63,11 +68,21 @@ function keyb.event(typ, code, val)
    elseif(typ == 1 and code == 1 and val == 1) then
       enter_text_mode = true
    elseif(typ == 1 and code == 14 and val == 1) then
-      text = string.sub(text, 1, -2)
+      -- text = string.sub(text, 1, -2)
+      text = text:sub(1, cursor-1) .. text:sub(cursor+1)
+      cursor = math.min(cursor-1, string.len(text))
+   elseif(typ == 1 and (code == 106 or code == 105) and val == 1) then
+      if(code == 105) then
+      cursor = math.max(cursor - 1, 1)
+      else 
+        cursor = math.min(cursor + 1, string.len(text) + 1)
+      end
    elseif(k ~= nil) then
-      text = text .. k
+      text = string.insert(text, k, cursor - 1)
+      --text = text .. k
+      cursor = cursor + 1
    end
-   -- print('typ ' .. typ .. ' code ' .. code .. ' val ' .. val)
+   --print('typ ' .. typ .. ' code ' .. code .. ' val ' .. val)
 end
 
 function init()
@@ -78,24 +93,25 @@ function init()
    clock.transport.start()
 end
 
-function string.insert(str1, str2, pos)
-    return str1:sub(1,pos)..str2..str1:sub(pos+1)
-end
-
 function redraw()
    screen.clear()
    if(enter_text_mode) then
-      screen.move(0,40)
-      screen.level(15)
-      local disp_text = text
-      if(string.len(disp_text)> 25) then
-          local line1 = disp_text:sub(1,25)
-          local line2 = disp_text:sub(26)
-          screen.text(line1)
-          screen.move(0, 56)
-          screen.text(line2)
-      else
-      screen.text(text)
+local current_y = 10
+     local current_x = 0
+      for i=1, string.len(text) do
+        if(i % 25 == 0) then
+          current_y = current_y + 16
+          current_x = 0
+          screen.move(0, current_y)
+        end
+          if(i == cursor) then
+                  screen.level(16)
+            else
+                screen.level(8)
+          end
+          screen.move(current_x, current_y)
+          screen.text(text:sub(i,i))
+          current_x = current_x + 5
       end
    else
       local x = 0
